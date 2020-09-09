2f41b9717d93eb903ce96681819b1fd2de084f71
Shrinath Prabhu <shrinath.prabhu@hashbinary.com>
2020-09-09 20:48:00 +0530

    Modified scripts to support initial prod build

--- package-scripts.js
+++ package-scripts.js
@@ -6,6 +6,19 @@ const { rimraf, crossEnv, series, concurrent } = npsUtils;
 
 module.exports = {
     scripts: {
+        build: {
+            description: 'Building in production environment.',
+            default: series.nps('clean', 'build.build'),
+            build: `parcel build src/bin/server.js --target node`,
+        },
+        clean: {
+            description: 'Clean dist folder.',
+            default: concurrent.nps(rimraf('dist'), rimraf('.cache')),
+        },
+        default: {
+            description: 'Start project with pm2 on production.',
+            script: `${crossEnv('NODE_ENV=production')} pm2 start processes.json`,
+        },
         dev: {
             start: {
                 description: 'Running on dev environment.',
@@ -16,8 +29,19 @@ module.exports = {
             },
             watch: {
                 description: 'Parcel watch for change and compile.',
-                script: 'NODE_ENV=development parcel watch src/bin/server.js --target node',
+                script: `${crossEnv('NODE_ENV=development')} parcel watch src/bin/server.js --target node`,
+            },
+            withDebug: {
+                script: `${crossEnv('NODE_ENV=development')} MONGOOSE_DEBUG=true DEBUG=express:* nodemon -r dotenv/config dist/server.js`,
+            },
+            debug: {
+                description: 'Running on dev environment with debug on.',
+                script: concurrent.nps('dev.watch', 'dev.withDebug'),
             },
         },
+        lint: {
+            default: 'eslint src',
+            fix: 'eslint --fix src',
+        }
     }
 }
