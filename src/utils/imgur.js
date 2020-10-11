72e83e15b2cd7d56abcd5b8bdbd5a89715d63364
Shrinath Prabhu <shrinath.prabhu@hashbinary.com>
2020-10-11 18:51:30 +0530

    Added Screenshot and imgur

--- src/utils/imgur.js
+++ src/utils/imgur.js
@@ -1,8 +1,28 @@
 import needle from 'needle';
+import { readFileSync } from 'fs';
 
 const clientId = process.env.IMGUR_CLIENTID;
 const clientSecret = process.env.IMGUR_CLIENTSECRET;
+const imgurApi = 'https://api.imgur.com/3';
 
-export async function saveImage() {
-    needle('')
+export async function saveImage(path) {
+  const response = await needle('post', `${imgurApi}/upload`, {
+    image: readFileSync(path),
+    type: 'file',
+  }, {
+    headers: {
+      Authorization: `Client-ID ${clientId}`,
+    },
+    multipart: true,
+  });
+  return response.body;
+}
+
+export async function deleteImage(deleteHash) {
+  const response = await needle('delete', `${imgurApi}/image/${deleteHash}`, {}, {
+    headers: {
+      Authorization: `Client-ID ${clientId}`,
+    },
+  });
+  console.log(response.body);
 }
