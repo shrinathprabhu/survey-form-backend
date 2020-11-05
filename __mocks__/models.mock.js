fbf1ffd72a57e485f65def6ba2c619bd4fc2c672
Shrinath Prabhu <shrinath.prabhu@hashbinary.com>
2020-11-05 17:25:29 +0530

    Added test cases

--- __mocks__/models.mock.js
+++ __mocks__/models.mock.js
@@ -1,4 +1,40 @@
 import mockingoose from "mockingoose";
+import { Types } from "mongoose";
 import { Form } from "../src/models/form.model";
 
 export let FormMock = mockingoose(Form);
+
+export let formsData = [
+    {
+        _id: Types.ObjectId(),
+        title: "Untitled Form",
+        description: "Some description",
+        questionnaires: [],
+        creator: {
+            uid: "somerandomuid",
+            ip: "127.0.0.1",
+            browser: "bot",
+            os: "mock-os",
+            userAgent: "bot (mock-os)",
+        },
+        status: "active",
+        createdAt: new Date(),
+        updatedAt: new Date()
+    },
+    {
+        _id: Types.ObjectId(),
+        title: "Untitled Form",
+        description: "Some description",
+        questionnaires: [],
+        creator: {
+            uid: "somerandomuid2",
+            ip: "192.168.0.1",
+            browser: "bot",
+            os: "mock-os",
+            userAgent: "bot (mock-os)",
+        },
+        status: "active",
+        createdAt: new Date(),
+        updatedAt: new Date()
+    },
+]
--- __tests__/models/form.model.test.js
+++ __tests__/models/form.model.test.js
@@ -1,40 +1,84 @@
-import { Types } from "mongoose";
-import { FormMock } from "../../__mocks__/models.mock";
+import { FormMock, formsData } from "../../__mocks__/models.mock";
 import * as FormModel from "../../src/models/form.model";
 
-test("Create a form document", async (done) => {
-    const createdForm = {
-        _id: Types.ObjectId(),
-        title: "Untitled Form",
-        description: "Some description",
-        questionnaires: [],
-        creator: {
-            uid: "somerandomuid",
-            ip: "127.0.0.1",
-            browser: "bot",
-            os: "mock-os",
-            userAgent: "bot (mock-os)",
-        },
-    };
-    FormMock.toReturn([], "find");
-    FormMock.toReturn(0, "countDocuments");
-    const list = await FormModel.list("somerandom", {});
-    expect(list).toHaveProperty("page", 1);
-    expect(list).toHaveProperty("size", 0);
-    expect(list).toHaveProperty("totalRecords", 0);
-    expect(list).toHaveProperty("pages", 0);
-    expect(list).toHaveProperty("list", []);
-    done();
+describe("Form creation works as expected", () => {
+    test("Creates a form document", async (done) => {
+        let createdForm = formsData[0];
+        FormMock.toReturn(createdForm, "save");
+        const form = await FormModel.create({
+            title: createdForm.title,
+            description: createdForm.description,
+            creator: createdForm.creator,
+        });
+        expect(form).toHaveProperty("id", createdForm._id);
+        expect(form).toHaveProperty("title", createdForm.title);
+        expect(form).toHaveProperty("description", createdForm.description);
+        expect(form).toHaveProperty("status", createdForm.status);
+        expect(form).toHaveProperty("createdAt", createdForm.createdAt);
+        done();
+    });
 });
 
-test("Returns blank list with pagination properties", async (done) => {
-    FormMock.toReturn([], "find");
-    FormMock.toReturn(0, "countDocuments");
-    const list = await FormModel.list("somerandom", {});
-    expect(list).toHaveProperty("page", 1);
-    expect(list).toHaveProperty("size", 0);
-    expect(list).toHaveProperty("totalRecords", 0);
-    expect(list).toHaveProperty("pages", 0);
-    expect(list).toHaveProperty("list", []);
-    done();
+describe("Form listing works as expected", () => {
+    test("Returns form list with pagination properties", async (done) => {
+        FormMock.toReturn(formsData, "find");
+        FormMock.toReturn(formsData.length, "countDocuments");
+        let mockResponse = formsData.map((form) => {
+            return {
+                id: form._id,
+                title: form.title,
+                description: form.description,
+            };
+        });
+        const list = await FormModel.list("somerandom", {});
+        expect(list).toHaveProperty("page", 1);
+        expect(list).toHaveProperty("size", 2);
+        expect(list).toHaveProperty("totalRecords", 2);
+        expect(list).toHaveProperty("pages", 1);
+        expect(list).toHaveProperty("list", mockResponse);
+        done();
+    });
+
+    test("Returns blank list with pagination properties", async (done) => {
+        FormMock.toReturn([], "find");
+        FormMock.toReturn(0, "countDocuments");
+        let mockResponse = [].map((form) => {
+            return {
+                id: form._id,
+                title: form.title,
+                description: form.description,
+            };
+        });
+        const list = await FormModel.list("somerandom", {});
+        expect(list).toHaveProperty("page", 1);
+        expect(list).toHaveProperty("size", 0);
+        expect(list).toHaveProperty("totalRecords", 0);
+        expect(list).toHaveProperty("pages", 0);
+        expect(list).toHaveProperty("list", mockResponse);
+        done();
+    });
+});
+
+describe("Access control for forms works as expected", () => {
+    test("can access if id and uid returns a form", async (done) => {
+        let accessibleForm = formsData[0];
+        FormMock.toReturn(accessibleForm, "findOne");
+        let canAccess = await FormModel.canAccess(
+            accessibleForm._id,
+            accessibleForm.creator.uid
+        );
+        expect(canAccess).toBeTruthy();
+        done();
+    });
+
+    test("cannot access if id and uid doesn't returns a form", async (done) => {
+        let accessibleForm = formsData[0];
+        FormMock.toReturn(undefined, "findOne");
+        let canAccess = await FormModel.canAccess(
+            accessibleForm._id,
+            accessibleForm.creator.uid
+        );
+        expect(canAccess).toBeFalsy();
+        done();
+    });
 });
